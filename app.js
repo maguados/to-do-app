6cc5d100a7d9f5473c380023874f5ee1574f9743
Miriam <maguadostaggs@ucdavis.edu>
2020-11-27 13:37:51 -0500

    New To Do w Array

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
-function onReady() {
-    const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
+window.onload = function () {
 
-    addToDoForm.addEventListener('submit', event => { //cacha el evento "submit"
-
-        event.preventDefault(); //para trabajar en una sola pagina y que no se recargue
-
-        if (newToDoText.value !== '') {
-
-        let title = newToDoText.value; // get the text
-        // create a new li
-        let newLi = document.createElement('li');
-        // create a new input
-        let checkbox = document.createElement('input');
-        // set the input's type to checkbox
-        checkbox.type = "checkbox";
-         //creates a delete button
-         let deleteBtn = document.createElement('button');
-         //Add text to the button
-         deleteBtn.textContent = 'Delete';
-         deleteBtn.setAttribute('class',"btn btn-danger");
-
-         deleteBtn.addEventListener('click', event=>{
-             //console.log(event);
-             //console/log(deleteBtn.parentElement);
-            toDoList.removeChild(deleteBtn.parentElement);
-         })
-        // set the title
-        newLi.textContent = title;
-        // attach the checkbox to the li
-        newLi.appendChild(checkbox);
-        //Attach the button to a list item
-        newLi.appendChild(deleteBtn);
-        // attach the li to the ul
-        toDoList.appendChild(newLi);
-        //empty the input
-        newToDoText.value = '';
-
-        }
-        else
-        alert("Add an element on your list.");
-
-    });
+    PaginaCargada();
 }
 
-window.onload =function () {
-    onReady();
- };
+function PaginaCargada() {
+    let toDos = [];
+
+    const addToDoForm = document.getElementById('addToDoForm');
+
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
+
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
+
+        //console.log(toDos);
+
+        newToDoText.value = '';
+
+        renderTheUI();
+    }
+
+    function renderTheUI() {
+        //Apunta al elemento ul
+        const toDoList = document.getElementById('toDoList');
+        //Limpas para que vuelva a dibujar.Sino los elementos de la lista se vana agregando una y otra vez
+        toDoList.textContent = '';
+
+        toDos.forEach(function (toDo) {
+            const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+
+            newLi.textContent = toDo.title;
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+        });
+
+    }
+
+    addToDoForm.addEventListener('submit', e => {
+        //Evita que se recargue la página en el Submit
+        e.preventDefault();
+        createNewToDo();
+    });
+
+    renderTheUI();
+
+}
